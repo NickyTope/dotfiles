0a2e31cd8197a0072e484ed4dbe7c5beb3503f82
NickyTope <nicky.tope@gmail.com>
2021-02-05 10:44:37 +1100

    zoom float

--- bspwm/.config/bspwm/zoom_float.sh
+++ bspwm/.config/bspwm/zoom_float.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-count=xprop _NET_WM_STATE | grep _NET_WM_STATE_ABOVE | wc -l
-[ $count = "1" ] && echo state=floating
+wid=$1
+# echo "init" > "/tmp/wid$wid.txt"
+props=`xprop -id "$1" | grep _NET_WM_STATE_ABOVE | wc -l`
+# echo "$props" > "/tmp/wid$wid.txt"
+[ "$props" -gt 0 ] && echo "state=floating"
