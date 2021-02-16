1173142487bf59d08337c949e570933f3e0ea711
NickyTope <nicky.tope@gmail.com>
2021-02-17 08:13:35 +1100

    float remmina main window

--- bspwm/.config/bspwm/external_rules.sh
+++ bspwm/.config/bspwm/external_rules.sh
@@ -2,6 +2,9 @@
 
 wid=$1
 # echo "init" > "/tmp/wid$wid.txt"
-props=`xprop -id "$1" | grep _NET_WM_STATE_ABOVE | wc -l`
+props=`xprop -id "$1"`
+above=`echo "$props" | grep _NET_WM_STATE_ABOVE | wc -l`
+remmina=`echo "$props" | grep "Remmina Remote Desktop Client" | wc -l` 
 # echo "$props" > "/tmp/wid$wid.txt"
-[ "$props" -gt 0 ] && echo "state=floating"
+[ "$above" -gt 0 ] && echo "state=floating"
+[ "$remmina" -gt 0 ] && echo "state=floating rectangle=600x600+0+0 center=on"
