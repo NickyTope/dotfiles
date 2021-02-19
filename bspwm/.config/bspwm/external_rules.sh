452fabb1166b08f7a64ab6a732a68d1efd11b024
NickyTope <nicky.tope@gmail.com>
2021-02-19 16:05:04 +1100

    float android emulator

--- bspwm/.config/bspwm/external_rules.sh
+++ bspwm/.config/bspwm/external_rules.sh
@@ -3,8 +3,10 @@
 wid=$1
 # echo "init" > "/tmp/wid$wid.txt"
 props=`xprop -id "$1"`
-above=`echo "$props" | grep _NET_WM_STATE_ABOVE | wc -l`
-remmina=`echo "$props" | grep "Remmina Remote Desktop Client" | wc -l` 
-# echo "$props" > "/tmp/wid$wid.txt"
+above=$(echo "$props" | grep _NET_WM_STATE_ABOVE | wc -l)
+remmina=$(echo "$props" | grep "Remmina Remote Desktop Client" | wc -l) 
+android=$(echo "$props" | grep "Emulator" | wc -l) 
+echo "$props" > "/tmp/wid$wid.txt"
 [ "$above" -gt 0 ] && echo "state=floating"
+[ "$android" -gt 0 ] && echo "state=floating"
 [ "$remmina" -gt 0 ] && echo "state=floating rectangle=600x600+0+0 center=on"
