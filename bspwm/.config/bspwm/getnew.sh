ad78c4b959e52d88da016a5e6915ec5589e55114
NickyTope <nicky.tope@gmail.com>
2021-08-09 14:34:41 +1000

    new galleries

--- bspwm/.config/bspwm/getnew.sh
+++ bspwm/.config/bspwm/getnew.sh
@@ -1,19 +1,16 @@
 #!/bin/bash
 
 # https://imgur.com/gallery/[id]
-# https://imgur.com/gallery/ft5IBjP
-# "0oiM1" "SPmIR" "lkhbk88" 
 # galleries=("rBarn" "TzfPU" "Gshpb" "T2kpv")
-galleries=("Y1OeM" "vdC7V" "OXhQa" "l8wwH" "ZpJXv" "8Yxub")
+# galleries=("Y1OeM" "vdC7V" "OXhQa" "l8wwH" "ZpJXv" "8Yxub")
+
+galleries=("SPmIR" "nyJ4M" "nyJ4M")
 gallery=${galleries[$RANDOM % ${#galleries[@]}]}
-echo $gallery
 IDS="$(curl "https://api.imgur.com/post/v1/posts/$gallery?client_id=883f0d26c1eb2b4&include=media,tags,account,adconfig,promoted" | jq '.media[].id' -r)"
-
-
 # IDS="$(wget -q "https://imgur.com/a/YsWMXAQ" -O - | grep post-image-container | grep class | cut -d\" -f 2)"
 
 readarray -t IDARRAY <<<"$IDS"
 ID=${IDARRAY[$RANDOM % ${#IDARRAY[@]}]}
-echo "fetching ${ID} from ${subreddit}"
+echo "fetching ${ID} from ${gallery}"
 cd ~/Pictures/walls/new && wget -c -q -N "https://i.imgur.com/$ID.jpg"
 
