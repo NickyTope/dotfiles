28c55393a2083d23bb4d674959ae57929356c428
Nicky Tope <nicky.tope@gmail.com>
2022-02-21 21:09:04 +1100

    more testing needed

--- nvim/.config/nvim/lua/test.lua
+++ nvim/.config/nvim/lua/test.lua
@@ -1,19 +1,42 @@
 local M = {}
 
-local P = function (v)
-  print(vim.inspect(v))
-  return v
+M.events = {}
+
+local function hasKey(map, key)
+  if map == nil then
+    return false
+  end
+
+  for _, value in ipairs(map) do
+    if key == value then
+      return true
+    end
+  end
+  return false
 end
 
 M.on_key = function (key)
-  local mode = vim.api.nvim_get_mode();
-  print(mode.mode .. "-" .. key)
+  local mode = vim.api.nvim_get_mode().mode;
+  if hasKey(M.keys[mode], key) then
+    table.insert(M.events, { mode = mode, key = key })
+  end
 end
 
-vim.on_key(M.on_key,0)
-
-local function name ()
-  print('hello')
+M.setup = function (opts)
+  M.keys = opts.keys or {}
+  M.ns = vim.on_key(M.on_key)
 end
+
+M.stop = function ()
+  vim.on_key(nil, M.ns)
+end
+
+M.setup({
+  keys = {
+    n = { "j", "k", "", "", "{", "}" }
+  }
+})
+
+-- vim.on_key(M.on_key,0)
 
 return M
