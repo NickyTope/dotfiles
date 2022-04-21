8853802399e283159b27673934a50fcedb10b938
NickyTope <nicky.tope@gmail.com>
2022-04-21 12:22:01 +1000

    choice_node for log snippet

--- nvim/.config/nvim/lua/snippets.lua
+++ nvim/.config/nvim/lua/snippets.lua
@@ -6,66 +6,100 @@ ls.config.set_config({
 	enable_autosnippets = true,
 })
 
+local t, i, c, f = ls.text_node, ls.insert_node, ls.choice_node, ls.function_node
+
 local same = function(index)
-	return ls.function_node(function(args)
+	return f(function(args)
 		return args[1]
 	end, { index })
 end
 
 local filename = function()
-	return ls.function_node(function(_args, snip)
-		local name = snip.snippet.env.TM_FILENAME
-		return string.sub(name, 1, string.find(name, ".", 1, true) - 1)
+	return f(function(_args, snip)
+		local name = vim.split(snip.snippet.env.TM_FILENAME, ".", true)
+		return name[1] or ""
 	end)
 end
 
 local log = ls.s(
 	"log",
-	fmt("console.log('{}', {});", {
-		ls.insert_node(1),
-		same(1),
+	fmt("console.log({});", {
+		c(1, {
+			i(1, "basic"),
+			ls.sn(1, { t("'"), i(1, "labelled"), t("', "), same(1) }),
+			ls.sn(1, { t("{ "), i(1, "object"), t(" }") }),
+		}),
 	})
 )
 
 local unwrap = ls.s(
 	"uw",
 	fmt("const {{ {} }} = {};", {
-		ls.insert_node(1, "key"),
-		ls.insert_node(2, "object"),
+		i(1, "key"),
+		i(2, "object"),
 	})
 )
 
 local useState = ls.s(
 	"us",
 	fmt("const [{}, {}] = useState({});", {
-		ls.insert_node(1),
-		ls.function_node(function(args)
+		i(1, "state"),
+		f(function(args)
 			local name = args[1][1]
 			local first = string.upper(string.sub(name, 1, 1))
 			local rest = string.sub(name, 2, -1)
 			return "set" .. first .. rest
 		end, { 1 }),
-		ls.insert_node(2),
+		i(0),
 	})
 )
 
 local fcomp = ls.s(
 	"fcomp",
-	fmt("export default function {}() {{\n\treturn (\n\t\t<div>hello {}</div>\n\t);\n}}", {
-		filename(),
-		filename(),
-	})
+	fmt(
+		[[
+  export default function {}({{ {} }}) {{
+    {}
+    return (
+      <div>hello {}</div>
+    );
+  }}
+  ]],
+		{
+			filename(),
+			i(1, "props"),
+			i(0),
+			filename(),
+		}
+	)
 )
 
-ls.snippets = {
-	all = {},
-	javascript = { log, unwrap, useState, fcomp },
-	typescript = { log, unwrap },
-	javascriptreact = { log, unwrap, useState, fcomp },
-}
+ls.add_snippets("javascript", { log, unwrap, useState, fcomp })
+ls.add_snippets("javascriptreact", { log, unwrap, useState, fcomp })
+ls.add_snippets("typescript", { log, unwrap })
 
 vim.keymap.set({ "i", "s" }, "<c-k>", function()
 	if ls.expand_or_jumpable() then
 		ls.expand_or_jump()
+	else
+		print("no snippet to expand")
 	end
 end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<c-l>", function()
+	if ls.choice_active() then
+		ls.change_choice(1)
+	else
+		print("no choice active")
+	end
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<c-h>", function()
+	if ls.choice_active() then
+		ls.change_choice(-1)
+	else
+		print("no choice active")
+	end
+end, { silent = true })
+
+ls.config.setup({})
