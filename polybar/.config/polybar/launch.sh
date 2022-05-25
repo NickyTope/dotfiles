b2a5fa1638e8d194df832c4a1b0138ebbceb992e
NickyTope <nicky.tope@gmail.com>
2022-05-25 10:41:52 +1000

    nightfox theme

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -4,7 +4,7 @@ killall polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-  MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload bottom &
+  MONITOR=$m polybar -c ~/.config/polybar/config.ini --reload top &
 done
 
-polybar main -c $(dirname $0)/config.ini &
+polybar main -c $(dirname $0)/config.ini --reload &
