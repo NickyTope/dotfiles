dfc242a15291494efcc8d7c82ef5bd7f3dd57be6
NickyTope <nicky.tope@gmail.com>
2022-07-04 15:34:16 +1000

    eww setup

--- eww/workspaces.sh
+++ eww/workspaces.sh
@@ -2,15 +2,20 @@
 
 workspaces() {
   all=$(bspc query -D --names)
-  box="(box :class \"ws\" :orientation \"v\""
+  focused=$(bspc query -D -d focused --names)
+  occupied=$(bspc query -D -d .occupied --names)
+  box="(box :class \"ws module\" :orientation \"v\""
 
   for name in $all
   do
-    focus=$(bspc query -D -d focused --names | grep "\\b$name\\b")
+    focus=$(echo "$focused" | grep "\\b$name\\b")
+    occ=$(echo "$occupied" | grep "\\b$name\\b")
     if [ $focus ]; then
-      box="$box (label :text \"$name\" :class \"focused\")"
+      box="$box (label :text \"$name\" :class \"ws-name focused\")"
+    elif [ $occ ]; then
+      box="$box (label :text \"$name\" :class \"ws-name occupied\")"
     else
-      box="$box (label :text \"$name\")"
+      box="$box (label :text \"$name\" :class \"ws-name\")"
     fi
     unset focus
   done
