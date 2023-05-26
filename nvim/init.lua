f92a45f11dc41c782bd2cda30e6145b2957a8d15
NickyTope <nicky.tope@gmail.com>
2023-05-26 12:03:33 +1000

    add browse for fugitive gh

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,5 +1,8 @@
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
+vim.cmd([[
+command! -nargs=1 Browse silent execute '!xdg-open' shellescape(<q-args>,1)
+]])
 
 require("ntope.settings")
 require("ntope.lazy")
